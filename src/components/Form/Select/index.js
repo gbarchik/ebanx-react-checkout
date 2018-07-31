bbcf4a47b019949efd011b6f2f14155c0c6da2a4
Guilherme Barchik <guilhermebarchik@Guilhermes-MacBook-Pro.local>
2018-07-31 18:50:17 -0300

    Updates Select dirty behavior

--- src/components/Form/Select/index.js
+++ src/components/Form/Select/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
 
@@ -26,7 +26,7 @@ const StyledSelect = styled.select`
     height: ${calculateRem(formInputHeight)};
     font-size: ${formInputFontSize};
     letter-spacing: ${formInputLetterSpacing};
-    color: ${(props) => props.dirty ? formInputTextColor : formInputPlaceholderColor};
+    color: ${(props) => props.isSelectDirty ? formInputTextColor : formInputPlaceholderColor};
     border-radius: 0;
     border: ${formInputBorder};
     background: ${formInputBackground} url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' version='1.1' width='32' height='24' viewBox='0 0 32 24'><polygon points='0,0 32,0 16,24' style='fill: rgb%28138, 138, 138%29'></polygon></svg>");
@@ -42,13 +42,32 @@ const StyledSelect = styled.select`
     }
 `
 
-const Select = (props) =>
-    <StyledSelect {...props}>
-        <option value="" disabled>{props.placeholder}</option>
-        {props.options && props.options.map((option) => (
-            <option key={`option-${option.val}`} value={option.val}>{option.name}</option>
-        ))}
-    </StyledSelect>
+class Select extends Component {
+    constructor(props) {
+        super(props)
+        this.state = { isSelectDirty: false }
+
+        this.handleSelectChange = this.handleSelectChange.bind(this)
+    }
+
+    handleSelectChange(e) {
+        this.props.onChange(e);
+        this.setState({ isSelectDirty: true })
+    }
+
+    render() {
+        const { props } = this
+
+        return (
+            <StyledSelect {...props} onChange={this.handleSelectChange} isSelectDirty={this.state.isSelectDirty}>
+                <option value="" disabled>{props.placeholder}</option>
+                {props.options && props.options.map((option) => (
+                    <option key={`option-${option.val}`} value={option.val}>{option.name}</option>
+                ))}
+            </StyledSelect>
+        )
+    }
+}
 
 Select.propType = {
     placeholder: PropTypes.string,
